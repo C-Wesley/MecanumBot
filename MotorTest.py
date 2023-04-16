57a282941e9f33a5c0da5155b15e3bd9c9ce9b19
Wesley <98190428+C-Wesley@users.noreply.github.com>
2023-04-16 12:12:53 -0400

    Testing Lidar in MotorTest

--- MotorTest.py
+++ MotorTest.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO # GPIO is used to access GPIO pins
 import math             # Math is math
 import hid              # Hid is for controller
 import sys              # sys is for system 
+from lidar_test import connect_to_lidar
 
 def setMotorDirection(fowardPin, backwardPin, isForwards):
 	""" 
@@ -335,7 +336,112 @@ def set_direction(forwardsPin, backwardsPin, power):
 	else:
 		setMotorDirection(forwardsPin, backwardsPin, False)
 	
-	
+def process_scans(scans):
+
+    #to set the cone range for movement
+    lowerBound = math.pi/4
+    upperBound = 3*lowerBound
+
+    #dict data structure to store angle as key and distance as value
+    dict = {}
+
+    #alg for storing angle and distance based on the 2 bounds, accounted for y axis inversion
+    for scan in scans:
+        for _, angle, dis in scan:
+            if(angle >= 5*lowerBound and angle <= 7*lowerBound): 
+                angle = 2*math.pi - angle        
+                dict[angle] = dis
+    return max(dict, key = dict.get) #returns the key with the maximum dict so it returns the angle
+
+def autonomous_movement():
+
+	while True:
+		
+		# Make sure we are stationary 
+		pwmA.ChangeDutyCycle(0)
+		pwmB.ChangeDutyCycle(0)
+		pwmC.ChangeDutyCycle(0)
+		pwmD.ChangeDutyCycle(0)
+
+		# Get the lidar object
+		lidar = connect_to_lidar()
+		scans = []
+		
+		try:
+			# Get the scans from the API
+			for indx, scan in enumerate(lidar.iter_scans()):
+				scans.append(scan)
+				# If we got 10 scans, break to update our plot
+				if indx > 10: 
+					break
+		
+			# Disconnect from the lidar. 
+			# I had issues trying to use the same lidar instance over time.
+			# so we will just re instantiate it each time we need it.
+			lidar.stop()
+			lidar.disconnect()
+
+			angle = process_scans(scans)
+
+			mag = 0.5
+			power1 = 100 * (math.sin(angle - (1/4)*math.pi) * mag)
+			power2 = 100 * (math.sin(angle + (1/4)*math.pi) * mag)
+									
+			set_direction(forwardA, backwardA, power2)
+			set_direction(forwardB, backwardB, power1)
+			set_direction(forwardC, backwardC, power2)
+			set_direction(forwardD, backwardD, power1)
+				
+			pwmA.ChangeDutyCycle(abs(power2))
+			pwmB.ChangeDutyCycle(abs(power1))
+			pwmC.ChangeDutyCycle(abs(power2))
+			pwmD.ChangeDutyCycle(abs(power1))
+
+			# let it travel for 2 seconds
+			time.sleep(2)
+
+		except KeyboardInterrupt:
+			print('Exiting for keyboard interrupt...') # good 
+			lidar.stop()
+			lidar.disconnect()
+			sys.exit(0)
+
+		except Exception as e:
+			print('Exiting for error...') # bad
+			print(e)                      # print error
+			lidar.stop()
+			lidar.disconnect()
+			sys.exit(1)
+
+
+
+
+
+	mag = 0.5
+    #may need some delays
+
+	angle = process_scans()
+
+	power1 = 100 * (math.sin(angle - (1/4)*math.pi) * mag)
+	power2 = 100 * (math.sin(angle + (1/4)*math.pi) * mag)
+							
+	set_direction(forwardA, backwardA, power2)
+	set_direction(forwardB, backwardB, power1)
+	set_direction(forwardC, backwardC, power2)
+	set_direction(forwardD, backwardD, power1)
+		
+	pwmA.ChangeDutyCycle(abs(power2))
+	pwmB.ChangeDutyCycle(abs(power1))
+	pwmC.ChangeDutyCycle(abs(power2))
+	pwmD.ChangeDutyCycle(abs(power1))
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
 	
 	# Try to init controller...
@@ -400,7 +506,7 @@ if __name__ == "__main__":
 	GPIO.output(enable_c, False)
 	GPIO.output(enable_d, False)
 	
-	actual_joystick()
+	#actual_joystick()
 	#mock_joystick()
 	#testPwm()
 	
